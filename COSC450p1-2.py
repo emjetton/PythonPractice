f7dbdd386853ab5cec590db53e4c8fad75849de9
emjetton <emjetton0@frostburg.edu>
2019-09-27 21:39:40 -0400

    Finished version

--- COSC450p1-2.py
+++ COSC450p1-2.py
@@ -10,8 +10,8 @@ print()
 print()
 
 matrix = []
-r = 3
-c = 3
+r = 5
+c = 5
 
 for i in range(r):
     arr = []
@@ -20,7 +20,7 @@ for i in range(r):
         arr.append(value1)
     matrix.append(arr)
     
-    
+print("matrix 1")  
 for i in range(r):
     for j in range(c):
         print(matrix[i][j], end = " ")
@@ -29,8 +29,8 @@ for i in range(r):
 print ()
 
 matrix2 = []
-r2 = 3
-c2 = 3
+r2 = r
+c2 = c
 
 for i in range(r2):
     arr2 = []
@@ -39,8 +39,27 @@ for i in range(r2):
         arr2.append(value1)
     matrix2.append(arr2)
     
-    
+print("matrix 2")  
 for i in range(r2):
     for j in range(c2):
         print(matrix2[i][j], end = " ")
     print()
+
+newMatrix = []
+for i in range(r):
+    mult = []
+    for j in range(c):
+        var1 = int(matrix[i][j])
+        var2 = int(matrix2[j][i])
+        var3 = var1*var2
+        mult.append(var3)
+    newMatrix.append(mult)
+
+print()
+print("multiplied matricies")  
+for i in range(r2):
+    for j in range(c2):
+        print(newMatrix[i][j], end = " ")
+    print()
+
+print()   
